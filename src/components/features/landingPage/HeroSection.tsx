2279645a3418261d0597a8d6270fcedf45358b3a
irorere juliet <irorerejuliet@gmail.com>
2026-03-06 12:11:21 +0100

    Added areact amination to the hero section

--- src/components/features/landingPage/HeroSection.tsx
+++ src/components/features/landingPage/HeroSection.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { heroimages } from "@/components/constant/heroimages";
 import Marquee from "react-fast-marquee";
+import { Slide } from "react-awesome-reveal";
 const HeroSection = () => {
   return (
     <>
-      {/* Hero */}
       <section className="min-h-screen bg-[url('/images/hero_image.svg')] bg-cover bg-center bg-no-repeat py-12 md:py-20 px-4 md:px-10">
+      <Slide direction="up">
         <div className="text-center mx-auto">
           <h1 className="text-4xl md:text-6xl font-bold lg:w-[809px] w-[325px] text-center mx-auto">
             Move From Idea to a <span className="text-primary">3triving</span>{" "}
@@ -17,6 +18,7 @@ const HeroSection = () => {
             Apps.
           </p>
         </div>
+        </Slide>
         {/* Bottons */}
         <div className="mt-10 flex flex-col sm:flex-row items-center justify-center gap-4 lg:hidden  block text-center mx-auto px-4">
           {/* Primary Button */}
