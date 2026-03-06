2e87f8587643e2cd42a37248b3ab37d0defa8e72
irorere juliet <irorerejuliet@gmail.com>
2026-03-06 17:39:01 +0100

    chnaged the image background on the  launching section

--- src/components/features/landingPage/ProfitPortfolio.tsx
+++ src/components/features/landingPage/ProfitPortfolio.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 
 const ProfitPortfolio = () => {
   return (
-    <section className=" py-12 md:py-38 px-4 ">
-      <div className="wrapper">
+    <section className=" py-12 md:py-38 ">
+      <div className="">
         <p className="text-xl   tracking-[0%] text-gray-400 mb-5">PORTFOLIO</p>
-        <div className='flex flex-col md:flex-row justify-between items-center'>
+        <div className="flex flex-col md:flex-row justify-center items-center">
           <h2 className="text-4xl md:text-5xl font-bold w-[343px] md:w-[709px]">
-            <span className="text-primary">Profit</span>{" "} and
+            <span className="text-primary">Profit</span> and
             <span className="text-primary"> 3triving</span> projects.
           </h2>
           <p className="text-gray-300 text-base md:text-xl w-[343px] md:w-[467px] leading-8 tracking-[0%] pt-4 md:pt-0">
@@ -22,14 +22,14 @@ const ProfitPortfolio = () => {
             alt="testimonial"
             width={672}
             height={513}
-            className=" "
+            className="ml-[55rem] "
           />
           <Image
             src="/images/hero-image3.svg"
             alt="testimonial"
             width={672}
             height={513}
-            className=" "
+            className=""
           />
         </div>
         <div className="flex items-center gap-5 pt-4">
